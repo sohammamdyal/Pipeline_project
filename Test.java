7fc2851d331f3e4244b73ddac67d8991ee9397c9
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-04-23 18:59:09 +0530

    edited few lines

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a test.");
+        System.out.println("Goodbye, World!");
+        System.out.println("This is a test.");
+
         // System.out.println("This is a test.");
         // System.out.println("Goodbye, World!");
     }
